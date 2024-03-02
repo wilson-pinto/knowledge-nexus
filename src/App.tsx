0318a5ba2aa484773ae2778124cebd2beac032a9
Wilson Pinto <wilson27pinto@gmail.com>
2024-03-02 11:45:52 +0530

    added scss and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 
 function App() {
   return (
-    <>
-    <h1>React app works</h1></>
+    <div className="bg-light py-5">
+      <div className="container">
+        <h1 className="display-6 text-dark">React app works</h1>
+        <p className="lead fw-semibold mt-3 text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis temporibus aut deserunt fuga, doloremque laboriosam vero perferendis dolore impedit accusamus?</p>
+      </div>
+    </div>
   );
 }
 
